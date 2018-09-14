52af810238925b6fef93ad6b18901c792e4edb43
Alejandro Osorio Trujillo <30884745+alejoso76@users.noreply.github.com>
2018-09-14 10:39:08 -0500

    modulo mayor

---
+++ modulo.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:10:47 09/14/2018 
+-- Design Name: 
+-- Module Name:    modulo - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+--todas las in y out
+entity modulo is
+end modulo;
+
+architecture Behavioral of modulo is
+
+signal PWM1, PWM2, PWM3;
+
+
+begin
+
+
+end Behavioral;
+
